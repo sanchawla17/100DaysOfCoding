21b95c346847881fda495202f429d1aa8fa9c7fa
Sanyam Chawla <91560018+sanchawla17@users.noreply.github.com>
2023-08-19 23:53:17 +0530

    Create maximumSubarray.cpp

---
+++ Day 7/maximumSubarray.cpp
@@ -0,0 +1,18 @@
+//53
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        return maxSubArray(nums, 0, size(nums)-1);
+    }
+    int maxSubArray(vector<int>& A, int L, int R){
+        if(L > R) return INT_MIN;
+        int mid = (L + R) / 2, leftSum = 0, rightSum = 0;
+        for(int i = mid-1, curSum = 0; i >= L; i--)
+            curSum += A[i],
+            leftSum=max(leftSum, curSum);
+        for(int i = mid+1, curSum = 0; i <= R; i++)
+            curSum += A[i],
+            rightSum = max(rightSum, curSum);        
+        return max({ maxSubArray(A, L, mid-1), maxSubArray(A, mid+1, R), leftSum + A[mid] + rightSum });
+    }	
+};
