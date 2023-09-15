caa437a5019686dc6245db0fd6466f70f9f413b9
Sanyam Chawla <91560018+sanchawla17@users.noreply.github.com>
2023-09-15 22:48:57 +0530

    Create MissungNumber.cpp

---
+++ Day 032/MissungNumber.cpp
@@ -0,0 +1,13 @@
+//268
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int ans=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            ans^=nums[i];
+            ans^=i+1;
+        }
+        return ans;
+    }
+};
