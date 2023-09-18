8132c33b19e990a5bc2d92c77eece8d2a989642b
Sanyam Chawla <91560018+sanchawla17@users.noreply.github.com>
2023-09-18 22:33:56 +0530

    Create SingleElementInASortedArray.cpp

---
+++ Day 035/SingleElementInASortedArray.cpp
@@ -0,0 +1,22 @@
+//540
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+      int n=nums.size();
+        if(n==1)return nums[0];
+        if(nums[0]!=nums[1])return nums[0];
+        if(nums[n-1]!=nums[n-2])return nums[n-1];
+        int st=1,end=n-2;
+        while(st<=end){
+            int mid=(st+end)/2;
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1])return nums[mid];
+
+            if((mid%2==1 && nums[mid]==nums[mid-1]) || (mid%2==0 && nums[mid]==nums[mid+1]) ){
+                st=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+        return -1;
+    }
+};
