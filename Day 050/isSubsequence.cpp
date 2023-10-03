fe219c14287cef208bb3e4c49af821c004efcba8
Sanyam Chawla <91560018+sanchawla17@users.noreply.github.com>
2023-10-03 23:09:24 +0530

    Create isSubsequence.cpp

---
+++ Day 050/isSubsequence.cpp
@@ -0,0 +1,12 @@
+//392
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int idx=0;
+        for(int i=0 ; i<t.size() ; i++){
+            if(t[i] == s[idx]) idx++;
+        }
+        if(idx == s.size()) return true;
+        else return false;
+    }
+};
